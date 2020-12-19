de265dbeaefceff6a53839e87cc6fd735b201ade
Arjun <44180430+ArjunSahlot@users.noreply.github.com>
2020-12-19 09:54:21 -0800

    Update constants.py

--- constants.py
+++ constants.py
@@ -1,3 +1,5 @@
+import os
+
 WIDTH, HEIGHT = 900, 600
 
 FPS = 90
@@ -9,3 +11,5 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
+
+PARDIR = os.path.realpath(os.path.dirname(__file__))
