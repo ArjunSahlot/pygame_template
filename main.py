b64825da5c864ec9c485323ac765bd7855adb53f
Arjun <44180430+ArjunSahlot@users.noreply.github.com>
2021-01-21 22:12:03 -0800

    Update main.py

--- main.py
+++ main.py
@@ -15,10 +15,16 @@ def main(window):
         clock.tick(FPS)
         window.fill(WHITE)
         events = pygame.event.get()
+        keys = pygame.key.get_pressed()
+        ctrl_pressed = keys[pygame.K_LCTRL] or keys[pygame.K_RCTRL]
         for event in events:
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q and ctrl_pressed:
+                    pygame.quit()
+                    return
         pygame.display.update()
 
 
